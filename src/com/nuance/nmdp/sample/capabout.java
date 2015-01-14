39527daff8b5eccb564e334d0b0d65f2dcf76265
pandachamp <pandachamp@me.com>
2015-01-14 17:25:46 +0700

    streamable

--- src/com/nuance/nmdp/sample/capabout.java
+++ src/com/nuance/nmdp/sample/capabout.java
@@ -1,19 +1,25 @@
 package com.nuance.nmdp.sample;
 
-import android.app.Activity;
+
 import android.content.Context;
-import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
-import android.view.View;
-import android.view.inputmethod.InputMethodManager;
-import android.widget.Button;
-import android.widget.EditText;
 
 /**
  * Created by admin on 1/14/15 AD.
  */
-public class capabout extends ActionBarActivity{
+public class capabout extends ActionBarActivity {
+    SharedPreferences mPrefs;
+    public void onCreate(Bundle savedInstanceState){
 
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.about);
+        Bundle extras = getIntent().getExtras();
+
+        if (extras != null) {
+            String para = extras.getString(DictationView.Byakko);
+        }
+        mPrefs = getSharedPreferences("gissmp3", Context.MODE_PRIVATE);
+    }
 }
