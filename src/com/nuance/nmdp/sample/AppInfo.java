80ec41d876c5419bd8356fe5e8abd3d501580eb5
pandachamp <pandachamp@me.com>
2015-01-14 09:07:54 +0700

    Initial commit

---
+++ src/com/nuance/nmdp/sample/AppInfo.java
@@ -0,0 +1,41 @@
+package com.nuance.nmdp.sample;
+
+public class AppInfo
+{
+    /**
+     * The login parameters should be specified in the following manner:
+     * 
+     * public static final String SpeechKitServer = "ndev.server.name";
+     * 
+     * public static final int SpeechKitPort = 1000;
+     * 
+     * public static final String SpeechKitAppId = "ExampleSpeechKitSampleID";
+     * 
+     * public static final byte[] SpeechKitApplicationKey =
+     * {
+     *     (byte)0x38, (byte)0x32, (byte)0x0e, (byte)0x46, (byte)0x4e, (byte)0x46, (byte)0x12, (byte)0x5c, (byte)0x50, (byte)0x1d,
+     *     (byte)0x4a, (byte)0x39, (byte)0x4f, (byte)0x12, (byte)0x48, (byte)0x53, (byte)0x3e, (byte)0x5b, (byte)0x31, (byte)0x22,
+     *     (byte)0x5d, (byte)0x4b, (byte)0x22, (byte)0x09, (byte)0x13, (byte)0x46, (byte)0x61, (byte)0x19, (byte)0x1f, (byte)0x2d,
+     *     (byte)0x13, (byte)0x47, (byte)0x3d, (byte)0x58, (byte)0x30, (byte)0x29, (byte)0x56, (byte)0x04, (byte)0x20, (byte)0x33,
+     *     (byte)0x27, (byte)0x0f, (byte)0x57, (byte)0x45, (byte)0x61, (byte)0x5f, (byte)0x25, (byte)0x0d, (byte)0x48, (byte)0x21,
+     *     (byte)0x2a, (byte)0x62, (byte)0x46, (byte)0x64, (byte)0x54, (byte)0x4a, (byte)0x10, (byte)0x36, (byte)0x4f, (byte)0x64
+     * };
+     * 
+     * Please note that all the specified values are non-functional
+     * and are provided solely as an illustrative example.
+     * 
+     */
+
+    /* Please contact Nuance to receive the necessary connection and login parameters */
+    public static final String SpeechKitServer = "sandbox.nmdp.nuancemobility.net";
+
+    public static final int SpeechKitPort = 443;
+    
+    public static final boolean SpeechKitSsl = false;
+
+    public static final String SpeechKitAppId = "NMDPTRIAL_mynameismaxz20141204080532";
+
+    public static final byte[] SpeechKitApplicationKey = {
+            (byte)0x18, (byte)0xc4, (byte)0x5a, (byte)0xab, (byte)0xdc, (byte)0x96, (byte)0xb8, (byte)0x67, (byte)0xf9, (byte)0x6a, (byte)0x9d, (byte)0x5d, (byte)0xc2, (byte)0xe5, (byte)0x44, (byte)0xe9, (byte)0x14, (byte)0xbd, (byte)0x6f, (byte)0x1a, (byte)0xa1, (byte)0xf6, (byte)0x19, (byte)0x8b, (byte)0xa7, (byte)0x20, (byte)0x47, (byte)0xe4, (byte)0x20, (byte)0xf3, (byte)0x1e, (byte)0x38, (byte)0xb7, (byte)0x79, (byte)0xe4, (byte)0x94, (byte)0x0f, (byte)0x9c, (byte)0xfb, (byte)0x08, (byte)0x07, (byte)0x98, (byte)0x66, (byte)0xc9, (byte)0x7e, (byte)0xaa, (byte)0xb2, (byte)0x92, (byte)0x67, (byte)0x22, (byte)0xad, (byte)0x08, (byte)0x18, (byte)0xc6, (byte)0x94, (byte)0xbf, (byte)0xe6, (byte)0xdb, (byte)0xa3, (byte)0x06, (byte)0xce, (byte)0xc2, (byte)0x3e, (byte)0x86
+    };
+}
